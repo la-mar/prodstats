c9da9dc0fab1e12570ef25a8fec29b3014d28ab0
Brock Friedrich <brocklfriedrich@gmail.com>
2020-03-08 15:14:10 -0500

    prod ingest schema

--- src/prodstats/schemas/prod.py
+++ src/prodstats/schemas/prod.py
@@ -1,8 +1,12 @@
 from datetime import date
+from typing import List, Optional
+
+import pandas as pd
+from pydantic import Field
 
 from schemas.bases import CustomBaseModel
 
-__all__ = ["ProdRecord", "ProdSet"]
+__all__ = ["ProdCalcRecord", "ProdCalcSet"]
 
 
 class ProdBase(CustomBaseModel):
@@ -11,28 +15,43 @@ class ProdBase(CustomBaseModel):
         # extra = "allow"
 
 
-class ProdRecord(ProdBase):
-    year: int
-    month: int
-    last_day: int
-    first_date: date
-    last_date: date
-    liquid: int
-    liquid_uom: str
-    oil: int
-    oil_uom: str
-    gas: int
-    gas_uom: str
-    casinghead_gas: int
-    casinghead_gas_uom: str
-    water: int
-    water_uom: str
-    gor: int
-    gor_uom: str
-    water_cut: float
-    well_count: int
-    oil_well_count: int
+class ProdCalcRecord(ProdBase):
+    """ Schema for production calculation input """
+
+    api10: str = Field(..., min_length=10, max_length=10)
+    api14: str = Field(..., min_length=14, max_length=14)
+    entity: str
+    entity12: str = Field(..., min_length=12, max_length=12)
+    # year: int
+    # month: int
+    prod_date: date = Field(..., alias="first_date")
+    days_in_month: Optional[int] = Field(..., alias="last_day")
+    # last_date: date
+    # liquid: Optional[int]
+    # liquid_uom: str
+    oil: Optional[int]
+    oil_uom: Optional[str]
+    gas: Optional[int]
+    gas_uom: Optional[str]
+    # casinghead_gas: Optional[int]
+    # casinghead_gas_uom: Optional[str]
+    water: Optional[int]
+    water_uom: Optional[str]
+    gor: Optional[int]
+    gor_uom: Optional[str]
+    water_cut: Optional[float]
+    perf_upper: Optional[int] = Field(..., alias="perf_upper_max")
+    perf_lower: Optional[int] = Field(..., alias="perf_lower_min")
+    perfll: Optional[int]
+    products: Optional[str]
+    wells: Optional[int]
 
 
-class ProdSet(ProdBase):
-    pass
+class ProdCalcSet(ProdBase):
+    production: List[ProdCalcRecord]
+
+    def dict(self) -> dict:
+        return super().dict()["production"]
+
+    def df(self) -> pd.DataFrame:
+        return pd.DataFrame(self.dict()).set_index(["api10", "prod_date"])
